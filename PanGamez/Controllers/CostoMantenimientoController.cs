c1472c95dad734c466dcef5962adc56e0d64496a
Cairo Rocha <cairor22@gmail.com>
2024-04-17 23:01:52 -0600

    costo de mantenimiento, vista, modelo y controlador no terminado

---
+++ PanGamez/Controllers/CostoMantenimientoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PanGamez.Models;
+
+namespace PanGamez.Controllers
+{
+    public class CostoMantenimientoController : Controller
+    {
+        // GET: CostoMantenimientoController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CostoMantenimientoController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CostoMantenimientoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CostoMantenimiento model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí implementa la lógica para calcular el costo de mantenimiento
+                // usando los datos proporcionados en el modelo
+                // y luego retorna la vista de resultados con el resultado del cálculo
+                return View("Index", model);
+            }
+            else
+            {
+                // Si el modelo no es válido, vuelve a mostrar el formulario con los errores
+                return View();
+            }
+        }
+    }
+    
+}
---
+++ PanGamez/Models/CostoMantenimiento.cs
@@ -0,0 +1,16 @@
+﻿namespace PanGamez.Models
+{
+    public class CostoMantenimiento
+    {
+        public double HorasTrabajo { get; set; }
+        public double Mtbf { get; set; }
+        public int NumeroFallas { get; set; }
+        public double DuracionTarea { get; set; }
+        public double CostoHoraTrabajo { get; set; }
+        public double Repuestos { get; set; }
+        public double TareasAdicionales { get; set; }
+        public double RetrasoLogistico { get; set; }
+        public double CostoUnitarioParada { get; set; }
+        public double CostoFallaUnica { get; set; }
+    }
+}
