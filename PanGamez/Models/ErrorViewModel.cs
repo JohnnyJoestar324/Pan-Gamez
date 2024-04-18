58006f3073defb434aca10181a2029ad9acad283
Cairo Manuel Rocha <88697565+Rcairo09@users.noreply.github.com>
2024-04-17 18:39:24 -0600

    Delete PanGamez directory

--- PanGamez/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PanGamez.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
