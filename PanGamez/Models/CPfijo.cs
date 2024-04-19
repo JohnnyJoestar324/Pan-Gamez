eff7a0ad3690787dad79f8763fd6ab455a19935f
Camilo Castro <castroh24@gmail.com>
2024-04-19 09:57:25 -0600

    clase rellenada

--- PanGamez/Models/CPfijo.cs
+++ PanGamez/Models/CPfijo.cs
@@ -2,5 +2,32 @@
 {
     public class CPfijo
     {
+        public int InventarioPromedio { get; set; }
+
+        public int VolumenOptimoDePedido { get; set; }
+
+        public int StockDeSeguridad { get; set; }
+
+        public int CalculoCP(int QQ, int SS)
+        {
+            //Primer plateamiento   
+            VolumenOptimoDePedido = QQ;
+            StockDeSeguridad = SS;
+
+            var resultado = (QQ / 2) + SS;
+            return resultado;
+
+
+        }
+        public int Rotacion(int IP)
+        {
+            InventarioPromedio = IP;
+            if (InventarioPromedio > 0)
+            {
+                return InventarioPromedio / 2;
+            }
+            else { return InventarioPromedio + 1; }
+
+        }
     }
 }
