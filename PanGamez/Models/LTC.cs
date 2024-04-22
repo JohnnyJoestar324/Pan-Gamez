aab5eb8ba8872675f4b857968fc1a4b02b2ef604
Cairo Rocha <cairor22@gmail.com>
2024-04-21 22:15:05 -0600

    imagen en carpeta y modelo LTC

---
+++ PanGamez/Models/LTC.cs
@@ -0,0 +1,11 @@
+﻿namespace PanGamez.Models
+{
+    public class LTC
+    {
+            public int Periodo { get; set; }
+            public int RequerimientoBruto { get; set; }
+            public double CostoOrdenar { get; set; }
+            public double CostoMantenimiento { get; set; }
+    }
+}
+
